3e77aa296d219e6340637feae6b3ec986ff5a58e
olivier poccard <olivierpoccard@mbp-de-olivier.lan>
2020-08-07 16:07:06 +0200

    Updates

---
+++ precache-manifest.14b8f53259a31e2f8a75f34f4e713573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37557e2b5245ef2e19894cafb7c2bec1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6792a2210a567ee6a74",
+    "url": "/static/css/main.23b7603c.chunk.css"
+  },
+  {
+    "revision": "b99c34e2bce2c1326fae",
+    "url": "/static/js/2.ec028354.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/static/js/2.ec028354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6792a2210a567ee6a74",
+    "url": "/static/js/main.98e15a22.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
